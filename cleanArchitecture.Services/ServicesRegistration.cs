566a8f9fed399d8cc620396e2aa90ac004f5a882
Abdallah Mohamed <abdallah.mohamed@flairstech.com>
2024-01-01 20:03:16 +0200

    servcie registration

--- cleanArchitecture.Services/ServicesRegistration.cs
+++ cleanArchitecture.Services/ServicesRegistration.cs
@@ -1,12 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using cleanArchitecture.Core.Interfaces;
+using cleanArchitecture.Services.services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace cleanArchitecture.Services
 {
     public static class ServicesRegistration
     {
+        public static void AddServicesRegistration(this WebApplicationBuilder Builder)
+        {
+
+            Builder.Services.AddTransient<ICarServices,CarServices>();
+        }
     }
 }

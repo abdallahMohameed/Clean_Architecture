4a66d1740c7741709cd90182782feaf4fe6933b1
Abdallah Mohamed <abdallah.mohamed@flairstech.com>
2024-01-01 17:44:03 +0200

    Register Repositort

--- cleanArchitecture.API/Program.cs
+++ cleanArchitecture.API/Program.cs
@@ -5,17 +5,14 @@ namespace cleanArchitecture.API
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
+            // services registration (IService collection)
 
             builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
---
+++ cleanArchitecture.Infrastructure/InfrastructureRegistration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Builder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cleanArchitecture.Infrastructure
+{
+    public class InfrastructureRegistration
+    {
+        public static void AddInfrastructureRegistration(this WebApplicationBuilder Builder)
+        {
+
+        }
+    }
+}

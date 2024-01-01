8fbd36f6e4f27520001b0ca0c368a839faed93c1
Abdallah Mohamed <abdallah.mohamed@flairstech.com>
2024-01-01 17:47:12 +0200

    Services Registration

--- cleanArchitecture.Infrastructure/InfrastructureRegistration.cs
+++ cleanArchitecture.Infrastructure/InfrastructureRegistration.cs
@@ -1,17 +1,16 @@
-﻿using Microsoft.AspNetCore.Builder;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using cleanArchitecture.Core.Interfaces;
+using cleanArchitecture.Infrastructure.Presistence;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace cleanArchitecture.Infrastructure
 {
-    public class InfrastructureRegistration
+    public static class InfrastructureRegistration
     {
         public static void AddInfrastructureRegistration(this WebApplicationBuilder Builder)
         {
-
+            
+            Builder.Services.AddTransient(typeof(IRepository<>),typeof(Repository<>));
         }
     }
 }
